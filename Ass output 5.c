58549675a5c5992b3fac70979190c4a83a42e105
sunkevishnuvardhan <128625764+sunkevishnuvardhan@users.noreply.github.com>
2023-03-26 20:28:31 +0530

    Create Ass output 5.c

---
+++ Ass output 5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int tmp = 20;
+
+void func();
+
+int main() {
+
+    printf("%d ", tmp);
+
+    func();
+
+    printf("%d ", tmp);
+
+    return 0;
+
+}
+
+void func() {
+
+    int tmp = 10;
+
+    printf("%d ", tmp);
+
+}
